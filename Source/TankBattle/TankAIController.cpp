01e5ae46a97d34e5ab6b872ae6858128608cb085
URSRAPID <98385291+URSRAPID@users.noreply.github.com>
2023-04-05 16:52:50 +0200

    fix_Error Move Ai Et Fire Balle

--- Source/TankBattle/TankAIController.cpp
+++ Source/TankBattle/TankAIController.cpp
@@ -25,15 +25,26 @@ void ATankAIController::Tick(float Deltatime)
 	Super::Tick(Deltatime);
 
 
+
 	APawn* PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	APawn* OwnTank = GetPawn();
 	auto AimingComponent = GetControlledAITank()->FindComponentByClass<UTankAimingComponent>();
 
+	/*ATank* PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());*/
+
+
 	if (!ensure(PlayerTank)) { return; }
+
 
 	AimingComponent->AimAt(PlayerTank->GetActorLocation(), AimingComponent->GetLaunchSpeed());
 	
 	AimingComponent->Fire();
+
+	//GetControlledAITank()->AimAt(GetControlledAITank()->GetActorLocation());
+	////UE_LOG(LogTemp, Warning, TEXT("tank player pos % s"), *GetPlayerTank()->GetActorLocation().ToString());
+	//GetControlledAITank()->Fire();
+
+	//if (!GetControlledAITank()) { return; }
 
 	MoveToActor(PlayerTank, AcceptanceRadius);
 }
